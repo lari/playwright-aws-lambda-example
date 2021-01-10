4366df96270a6ae06e197a95df9589375fae7623
Lari Haataja <lari.haataja@gmail.com>
2021-01-10 23:36:01 +0200

    Initial code

---
+++ Dockerfile
@@ -0,0 +1,48 @@
+# Define custom function directory
+ARG FUNCTION_DIR="/function"
+
+FROM mcr.microsoft.com/playwright:focal as build-image
+
+# Include global arg in this stage of the build
+ARG FUNCTION_DIR
+
+# Install build dependencies
+RUN apt-get update && \
+    apt-get install -y \
+    ffmpeg \
+    g++ \
+    make \
+    cmake \
+    unzip \
+    libcurl4-openssl-dev \
+    autoconf \
+    libtool
+
+# Create function dir and set it as WORKDIR
+RUN mkdir -p ${FUNCTION_DIR}
+WORKDIR ${FUNCTION_DIR}
+
+# Install npm packages
+# COPY will invalidate the cache for following instructions if the contents of <src> have changed,
+# so in order to optimize the build we'll copy only package.json and run npm install first
+COPY function/package.json ${FUNCTION_DIR}
+RUN npm install
+
+# Install AWS Lambda Node.js Runtime Interface Client API
+RUN npm install aws-lambda-ric
+
+# Copy the rest of the function directory
+COPY function/ ${FUNCTION_DIR}
+
+# Copy browser executables outside user cache
+RUN mv /root/.cache/ms-playwright /
+
+# Add Lambda Runtime Interface Emulator and use a script in the ENTRYPOINT for simpler local runs
+ADD https://github.com/aws/aws-lambda-runtime-interface-emulator/releases/latest/download/aws-lambda-rie /usr/local/bin/aws-lambda-rie
+RUN chmod 755 /usr/local/bin/aws-lambda-rie
+COPY entrypoint.sh /
+RUN chmod 755 /entrypoint.sh
+ENTRYPOINT [ "/entrypoint.sh" ]
+
+# Set function handler
+CMD ["index.handler"]
---
+++ function/index.js
@@ -0,0 +1,47 @@
+const { webkit, chromium, firefox } = require('playwright');
+
+
+getCustomExecutablePath = (expectedPath) => {
+    const suffix = expectedPath.split('/.cache/ms-playwright/')[1];
+    return  `/ms-playwright/${suffix}`;
+}
+
+exports.handler = async (event, context) => {
+    const browserName = event.browser || 'chromium';
+    const browserTypes = {
+        'webkit': webkit,
+        'chromium': chromium,
+        'firefox': firefox
+    };
+    const browserLaunchArgs = {
+        'webkit': [],
+        'chromium': [
+            '--disable-dev-shm-usage',
+            '--no-sandbox',
+            '--no-zygote',
+            '--use-gl=swiftshader',
+            '--in-process-gpu',
+            '--single-process',
+        ],
+        'firefox': []
+    }
+    let browser = null;
+    try {
+        console.log(`Starting browser: ${browserName}`);
+        browser = await browserTypes[browserName].launch({
+            executablePath: getCustomExecutablePath(browserTypes[browserName].executablePath()),
+            args: browserLaunchArgs[browserName],
+        });
+        const context = await browser.newContext();
+        const page = await context.newPage();
+        await page.goto('http://google.com/');
+        console.log(`Page title: ${await page.title()}`);
+    } catch (error) {
+        console.log(`error${error}`);
+        throw error;
+    } finally {
+        if (browser) {
+            await browser.close();
+        }
+    }
+}
